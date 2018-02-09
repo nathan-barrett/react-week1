6e23ccfdb8066141a5ccde6c26ce7ebb805a9db6
Nathan Barrett <demeules.barrett@gmail.com>
2018-02-09 14:44:35 -0800

    got images to work for the Inventory Display

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Inventory = (props) => {
+function Inventory(props) {
+
   return (
     <div className='inventory-container'>
-      <h3>{props.name}</h3>
-      <img src={props.image}/>
-      <p>Price: {props.price}</p>
-      <p>Farm: {props.farm}</p>
-      <p>Energy Level: props.energy</p>
+      <h3 className='full-width'>{props.name}</h3>
+      <img src={props.image} />
+      <p className='full-width'>Price: {props.price}</p>
+      <p className='full-width'>Farm: {props.farm}</p>
+      <p className='full-width'>Energy Level: {props.energy}</p>
+      <style jsx>{`
+        .inventory-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-wrap: wrap;
+          border: 2px solid black;
+          box-shadow: 2px 2px 5px grey;
+          width: 400px;
+          height: 300px;
+          margin-top: 2vh;
+          flex-direction: column;
+        }
+        .full-wdith {
+          width: 100%;
+        }
+        img {
+          max-width: 100%
+        }
+      `}
+      </style>
     </div>
   );
-};
+}
 
 Inventory.propTypes = {
   name: PropTypes.string.IsRequired,
   price: PropTypes.number.IsRequired,
   farm: PropTypes.string.IsRequired,
   energy: PropTypes.number.IsRequired,
-  image: PropTypes.string
+  image: PropTypes.any
 };
 
 export default Inventory;
--- src/components/InventoryList.jsx
+++ src/components/InventoryList.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import Inventory from './Inventory';
+import Sugar from '../../assets/images/Sugar.png';
+import Rice from '../../assets/images/Rice.png';
+import Strawberry from '../../assets/images/Strawberry.png';
+import Pepper from '../../assets/images/Pepper.png';
+import Eggplant from '../../assets/images/Eggplant.png';
+import Milk from '../../assets/images/Milk.png';
+
 
 const masterInventory = [
   {
@@ -7,48 +14,55 @@ const masterInventory = [
     price: 325,
     farm: 'Marnie',
     energy: 25,
-    image: 'assets/img/Sugar.png'
+    image: Sugar
   },
   {
     name: 'Rice',
     price: 200,
     farm: 'George',
     energy: 13,
-    image: 'assets/img/Rice.png'
+    image: Rice
   },
   {
     name: 'Strawberry',
     price: 120,
     farm: 'Egg Festival',
     energy: 25,
-    image: 'assets/img/Strawberry.png'
+    image: Strawberry
   },
   {
     name: 'Hot Peppers',
     price: 40,
     farm: 'Shane',
     energy: 50,
-    image: 'assets/img/Pepper.png'
+    image: Pepper
   },
   {
     name: 'Eggplant',
     price: 100,
     farm: 'Pam',
     energy: 20,
-    image: 'assets/img/Eggplant.png'
+    image: Eggplant
   },
   {
     name: 'Goat Milk',
     price: 500,
     farm: 'Pam',
     energy: 500,
-    image: 'assets/img/Milk.png'
+    image: Milk
   }
 ];
 
 const InventoryList = () => {
+  const flexStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignContent: 'center',
+    flexWrap: 'wrap',
+
+  };
   return (
-    <div>
+    <div style={flexStyle}>
       {masterInventory.map((inventory, i) =>
         <Inventory
           name={inventory.name}
